cc29c9abefa6001b893ab45c1a19cba7224a1efe
LrnzDC3696 <lrnzdc3696+github@gmail.com>
2023-07-05 08:29:29 +0800

    Added a test, and vampy to poetry

---
+++ tests/test_entity.py
@@ -0,0 +1,16 @@
+import vampytest
+from margelet import TextEntity
+
+
+example_test_entity_type = "plain"
+example_test_entity_text = "plain"
+example_text_entity = {
+    "type": example_test_entity_type,
+    "text": example_test_entity_text
+}
+
+
+def test_text_entity():
+    entity_init = TextEntity(example_test_entity_type, example_test_entity_text)
+    entity_from_dict = TextEntity.from_dict(example_text_entity)
+    vampytest.assert_eq = (entity_init, entity_from_dict)
